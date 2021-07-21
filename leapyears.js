f914e6f460fa14dcfe8571805bf9cc3e99327106
[K H] <[epixbubble+public@gmail.com]>
2021-07-21 17:41:08 -0400

    general improvements

--- leapyears.js
+++ leapyears.js
@@ -2,29 +2,54 @@ var startYear;
 var endYear;
 var leapYears = [];
 var type = ", ";
-var pageOutput = document.getElementById("output");
+var pageOutput     = document.getElementById("output");
+var popupElement   = document.getElementById("popup");
+var darkenElement  = document.getElementById("darken");
+var submitButton   = document.getElementById("submit");
+var warningElement = document.getElementById("warning");
 
-promptInput();
-calculate();
+submitButton.addEventListener("click", calculate);
+darkenElement.addEventListener("click", popup);
 
-// Prompt
-function promptInput() {
-    startYear = prompt("Please enter a starting year", "1900");
-    endYear = prompt("Please enter an end year", "2100");
-    calculate();
-}
+
+// promptInput();
+// // calculate();
+
+// // Prompt
+// function promptInput() {
+//     startYear = prompt("Please enter a starting year", "1900");
+//     endYear = prompt("Please enter an end year", "2100");
+//     calculate();
+// }
 
 // For loop leap year calculator
 function calculate() {
+
+    let startYear = parseInt(document.getElementById("startYear").value);
+    let endYear = parseInt(document.getElementById("endYear").value);
+
     leapYears = [];
-    for(let i = startYear; i <= endYear; i++) {
+
+    // Error message
+    if(startYear >= endYear) {
+        warningElement.style.visibility = "inherit";
+        warningElement.style.animation = "floatin 0.25s"
+    } else {
+        warningElement.style.visibility = "hidden";
+        warningElement.style.animation = "none"
+    }
+
+    // For loop
+    for(i = startYear; i <= endYear; i++) {
         if(  (i % 4 == 0 && i % 100 !== 0)  ||  (i % 400 == 0)  ) {
             leapYears.push(i);
         }
         if(i == endYear) {
+            popup();
             pageUpdate();
         }
     }
+
 }
 
 // Update page
@@ -40,8 +65,27 @@ function displayType() {
         pageUpdate();
     } else if(type == "\n") {
         type = ", ";
-        pageOutput.style.fontSize = "12pt";
+        pageOutput.style.fontSize = "14pt";
         pageUpdate();
+    }
+}
+
+// Hide and unhide popup menu
+function popup() {
+    if(popupElement.style.visibility == "inherit") {
+        popupElement.style.animation = "fadeout 0.25s";
+        darkenElement.style.animation = "opacityout 0.25s";
+
+        setTimeout(() => {
+            popupElement.style.visibility = "hidden";
+            darkenElement.style.visibility = "hidden";
+        }, 200);
+    } else {
+        popupElement.style.visibility = "inherit";
+        darkenElement.style.visibility = "inherit";
+
+        popupElement.style.animation = "fadein 0.25s";
+        darkenElement.style.animation = "opacityin 0.25s";
     }
 }
 
